c49a91ec95d40652c9984a00e3883871a1420ce9
njokinjuguna <ntikinjuguna@gmail.com>
2025-04-30 19:02:09 +0200

    Fix uvicorn run method to direct app import

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 import uvicorn
+from mira.api.main import app  # <-- Directly import the FastAPI app
 
 if __name__ == "__main__":
-    uvicorn.run("mira.api.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
